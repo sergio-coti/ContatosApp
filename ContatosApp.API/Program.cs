3a7eb570155d2a637b779b92a3daa14595aabb8d
sergio-coti <cotiaulajava@outlook.com>
2024-02-23 12:48:03 -0300

    configurações de cors e serviços da api

--- ContatosApp.API/Program.cs
+++ ContatosApp.API/Program.cs
@@ -7,6 +7,18 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+//definindo a politica de permissão do CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("DefaultPolicy",
+        builder =>
+        {
+            builder.AllowAnyOrigin() //qualquer projeto poderá acessar a API
+                   .AllowAnyMethod() //permissão para qualquer método POST, PUT, DELETE, GET
+                   .AllowAnyHeader(); //permissão para envio de dados no cabeçalho das requisições
+        });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -17,6 +29,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseAuthorization();
+
+//adicionando a politica
+app.UseCors("DefaultPolicy");
 
 app.MapControllers();
 
